cd3105af11ef252d7c5530659a36142202d7b4bf
Phyllis <plynch2@mail.immaculata.edu>
2022-02-24 23:46:09 -0500

    lab4

--- lab4.py
+++ lab4.py
@@ -3,15 +3,13 @@ my_dict = {"winter":"a cold season", "spring":"a wet season",
            "summer":"the only season worth talking about"}
 while True:
     response = (input( "What season are you looking for? "))
-    if response in my_dict:
-        print("that is in the dictionary")
-        continue
+    if my_dict.get(response):
+        print (my_dict.get(response), "is in the dictionary")
     else: response = (input("That is not found do you want to add it? yes or no "))
     if response ==("no"):
-        print("don't bother me")
-    elif response == ("yes"):
-        '''my_dict.update'''
-    key = (input("type the season name "))
-    value = (input("type the season description "))
-    my_dict[key]=(value)
-    print(my_dict)
+        print (my_dict.get(response), "don't bother me")
+    if response == ("yes"):
+        key = (input("type the season name "))
+        value = (input("type the season description "))
+        my_dict[key]=(value)
+        print(my_dict)
