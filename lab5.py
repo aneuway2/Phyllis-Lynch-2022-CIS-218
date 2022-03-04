1d0a043e3adbe3771c16bf09e205779d936faed2
Phyllis <plynch2@mail.immaculata.edu>
2022-03-03 21:29:45 -0500

    revision

--- lab5.py
+++ lab5.py
@@ -1,20 +1,22 @@
-'''Phyllis Lynch lab 5'''
-def ce_to_fa(celsius):
-    '''calc celcius to fahreheit'''
-    fahrenheit = celsius * 9/5 + 32
+"""Phyllis Lynch lab 5"""
+
+
+def celsius_to_fahrenheit(celsius=0):
+    """calc celcius to fahreheit"""
+    fahrenheit = celsius * 9 / 5 + 32
     return fahrenheit
-def test_ce_to_fa():
+
+
+def test_celsius_to_fahrenheit():
     """check the input"""
-    assert ce_to_fa(0) == (32)
-    assert ce_to_fa() == (0)
-temp = float(input("what is the temperature in celcius? "))
-print("temp in fahrenheit is", ce_to_fa(temp))
-hi_low ={"32": "fahrenheit freezing", "212":"farenheit boiling"}
-value = (hi_low.get(temp,"nope"))
+    assert celsius_to_fahrenheit(0) == (32)
+    assert celsius_to_fahrenheit() == (0)
+    assert celsius_to_fahrenheit(1 <= temp <= 99), "not freezing or boiling"
+    assert celsius_to_fahrenheit(101 <= temp <= 200), "steaming"
+
+
+temp = float(input("what is the temperature in celsius? "))
+print("temp in fahrenheit is", celsius_to_fahrenheit(temp))
+hi_low = {"32": "fahrenheit freezing", "212": "farenheit boiling"}
+value = hi_low.get("32") or hi_low.get("212")
 print(value)
-if ce_to_fa(temp) == 32:
-    print("that is freezing")
-elif ce_to_fa(temp) == 212:
-    print("that is boiling")
-else:
-    print("that is not boiling or freezing")
