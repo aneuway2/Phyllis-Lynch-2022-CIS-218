d6248e8777cd5d461f965760674fa59b3ed2e0b0
Phyllis <plynch2@mail.immaculata.edu>
2022-03-20 09:05:59 -0400

    revision

--- lab7.py
+++ lab7.py
@@ -4,29 +4,29 @@
 def calculate_bmi(height, weight):
     """calculate bmi from inout"""
     bmi = round(weight / height / height * 703, 1)
-    print(f"the bmi is", bmi, "which is ", category)
+    print("The bmi is", bmi, "which is ", category)
     return bmi
 
 
 def test_calculate_bmi(bmi):
-    assert calculate_bmi(59, 143), 28.9
-    assert calculate_bmi(63, 200), 35.4
-    assert calculate_bmi(50, 215), 60.5
-    assert calculate_bmi(48, 95), 29.0
+    assert calculate_bmi(59, 143) == 28.9
+    assert calculate_bmi(63, 200) == 35.4
+    assert calculate_bmi(50, 215) == 60.5
+    assert calculate_bmi(48, 95) == 29.0
 
 
-def bmi_to_category(bmi):
-    if bmi < 18.5:
+def bmi_to_category(cat):
+    if 18.5 < bmi:
         return "underweight"
-    if bmi > 18.5 < 24.9:
+    if  18.5 > bmi < 24.9:
         return "healthy"
-    if bmi > 24.9 < 29.9:
+    if 24.9 > bmi < 29.9:
         return "unhealthy"
-    if bmi > 29.9:
+    if 29.9 > bmi:
         return "Obese"
 
 
-def test_bmi_to_category(category):
+def test_bmi_to_category(bmi):
     assert bmi_to_category(10) == "underweight"
     assert bmi_to_category(23) == "healthy"
     assert bmi_to_category(26) == "unhealthy"
@@ -38,15 +38,15 @@ weight = float(input("Enter your weight in pounds please. "))
 bmi = round(weight / height / height * 703, 1)
 if bmi:
     if bmi < 18.5:
-        category = "underweight"
+        category = "Underweight"
     if bmi > 18.5 < 24.9:
         category = "healthy"
     if bmi > 24.9 < 29.9:
         category = "unhealthy"
     if bmi > 29.9:
-        category = "Obese"
+        category = "obese"
     bmi_to_category(bmi)
 bmi = calculate_bmi(height, weight)
 cat = bmi_to_category(bmi)
-test_calculate_bmi(height, weight)
-test_bmi_to_category(category)
+'''test_calculate_bmi(bmi)
+test_bmi_to_category(category)'''
